16b8fb3d70b1274b575b8c64b4ab1ab934d686c4
Theanhmgt <theanhmgt66@gmail.com>
2024-01-11 18:01:55 +0700

    TC3_2_2_1 Register with correct values

---
+++ msistore_testing/Register.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+namespace msistore_testing
+{
+    [TestClass]
+    public class Register
+    {
+        private IWebDriver driver;
+        private string baseUrl;
+        [TestInitialize]
+        public void setUp()
+        {
+            driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            baseUrl = "http://localhost:3000";
+        }
+        [TestMethod]
+        public void TC3_2_2_1()
+        {
+            driver.Navigate().GoToUrl(baseUrl + "/register");
+            IWebElement firstNameInput = driver.FindElement(By.CssSelector("input[name='first_name']"));
+            IWebElement lastNameInput = driver.FindElement(By.CssSelector("input[name='last_name']"));
+            IWebElement emailInput = driver.FindElement(By.CssSelector("input[name='email']"));
+            IWebElement usernameInput = driver.FindElement(By.CssSelector("input[name='username']"));
+            IWebElement passwordInput = driver.FindElement(By.CssSelector("input[name='password']"));
+            IWebElement conFirmPasswordInput = driver.FindElement(By.CssSelector("input[name='confirmPassword']"));
+            IWebElement avatarInput = driver.FindElement(By.CssSelector("input[name='avatar']"));
+            IWebElement signInBTN = driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div[2]/div/form/div[8]/button"));
+
+            firstNameInput.SendKeys("Anh");
+            lastNameInput.SendKeys("Nguyễn");
+            emailInput.SendKeys("abc@gmail.com");
+            usernameInput.SendKeys("theanh28");
+            passwordInput.SendKeys("Theanh28");
+            conFirmPasswordInput.SendKeys("Theanh28");
+            avatarInput.SendKeys("C:\\Users\\Admin\\Pictures\\Personal\\selenium.png");
+            signInBTN.Click();
+
+            Thread.Sleep(6000);
+
+            string current_url = driver.Url;
+            string home_url = baseUrl + "/";
+            Assert.AreEqual(home_url,current_url);
+        }
+
+  
+        [TestCleanup] 
+        public void TestDown()
+        {
+            driver.Quit();
+        }
+    }
+}
